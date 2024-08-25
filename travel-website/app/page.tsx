8c800f229131729ddcf2faa4dbbc5dbf6c0a46a6
Jerin Andrews <jerinandrews125@gmail.com>
2024-08-25 00:03:37 -0500

    Updated Text
    - Added Landing Page Text

--- travel-website/app/page.tsx
+++ travel-website/app/page.tsx
@@ -1,20 +1,25 @@
 import Image from "next/image";
 import Navbar from "./navbar";
-import { Box } from "@mui/joy";
+import { Box, Typography } from "@mui/joy";
 import AspectRatio from "@mui/joy";
 
 export default function Home() {
   return (
     <Box>
       <Navbar/>
-      <img
-        style={{
-          maxWidth: "100%",
-          marginTop: "-50px"
-        }}
-        src="home_page_img.jpeg"
-        alt=""
-      />
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <img
+          style={{
+            maxWidth: "100%",
+            marginTop: "-50px"
+          }}
+          src="home_page_img.jpeg"
+          alt=""
+        />
+        <Typography level="h1" sx={{color: "#F7F8FA", ml: "-600px", mt: "-150px"}} position="absolute">Travel With Us</Typography>
+        <Typography level="title-lg" sx={{color: "#F7F8FA", ml: "-440px", mt: "-50px"}} position="absolute">Say hello to the world with our Package Plans!</Typography>
+      </Box>
+
     </Box>
   );
 }
